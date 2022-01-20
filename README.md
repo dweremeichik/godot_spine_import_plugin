aa828020404a985d8baf370fbbcac833407045bc
Dylan Weremeichik <dylan.weremeichik@gmail.com>
2022-01-20 02:42:24 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Spine Importer
+
+<!-- ![Icon](https://raw.githubusercontent.com/dweremeichik/godot_spine_import_plugin/main/asset_lib/asset_lib_icon.png) -->
+
+Spine Importer is a plugin for the [Godot Engine](https://godotengine.org). 
+It is designed to make 2D rigging significantly faster and more accurate by placing your rigging textures for you.
+
+## How to install
+
+Official installation instructions may be found [here](https://docs.godotengine.org/en/stable/tutorials/plugins/editor/installing_plugins.html#installing-a-plugin).
+You may either install through the Godot Asset Library, or you can download a zip file directly from GitHub.
+
+## How to use
+
+To use this plugin simply enable it, you can find the official instructions [here](https://docs.godotengine.org/en/stable/tutorials/plugins/editor/installing_plugins.html#enabling-a-plugin).
+Add your images/textures to your project along with your Spine.json file. Your Spine.json file should now behave like a Scene inside of the godot editor. If everything worked well, you should see your sprites laid out appropriately.
+
+> Note: You may need to re-import your Spine.json file from the import dock if your Spine.json file was imported before your images/textures were.
+
+
+## License
+
+This plugin is MIT licensed. The license file is located at [addons\spine_importer\LICENSE](https://github.com/dweremeichik/godot_spine_import_plugin/blob/main/addons/spine_importer/LICENSE).
---
+++ addons/spine_importer/import_plugin.gd
@@ -0,0 +1,85 @@
+tool
+extends EditorImportPlugin
+
+enum Presets { DEFAULT}
+
+
+func get_importer_name() -> String:
+	return "geekforged.spine.json"
+
+
+func get_visible_name() -> String:
+	return "Scene"
+
+
+func get_recognized_extensions() -> Array:
+	return ["json"]
+
+
+func get_save_extension() -> String:
+	return "tscn"
+
+
+func get_resource_type() -> String:
+	return "PackedScene"
+
+
+func get_preset_count() -> int:
+	return Presets.size()
+
+
+func get_preset_name(preset: int) -> String:
+	 return Presets.keys()[preset].capitalize()
+
+
+func get_import_options(_preset: int) -> Array:
+	return []
+
+
+func get_option_visibility(_option: String, _options: Dictionary) -> bool:
+	return true
+
+
+func import(source_file: String, save_path: String, options: Dictionary, platform_variants: Array, gen_files: Array) -> int:
+		var file = File.new()
+		var file_err = file.open(source_file, File.READ)
+		if file_err != OK:
+			return file_err
+		
+		var file_content = file.get_as_text()
+		
+		file.close()
+		
+		var json_parse = JSON.parse(file_content)
+		if json_parse.error != OK:
+			return json_parse.error
+		
+		var root_node = Node2D.new()
+		root_node.name = "root"
+		
+		var skins = json_parse.result.skins
+		var image_path = source_file.rsplit("/", false, 1)[0]
+#		print(image_path)
+		for skin in skins:
+			for slot in skins[skin]:
+				print("Linking spine texture %s/%s.png" % [image_path, slot])
+				var texture = load("%s/%s.png" % [image_path, slot])
+				if texture:
+					var sprite = Sprite.new()
+					sprite.texture = texture
+					sprite.name = slot
+					sprite.centered = false
+					sprite.offset.x = skins[skin][slot][slot].width / -2
+					sprite.offset.y = skins[skin][slot][slot].height / -2
+					sprite.position.x = skins[skin][slot][slot].x
+					sprite.position.y = skins[skin][slot][slot].y * -1
+					root_node.add_child(sprite)
+					sprite.owner = root_node
+					
+					
+		var scene = PackedScene.new()
+		var packing_err = scene.pack(root_node)
+		if packing_err != OK:
+			return packing_err
+		
+		return ResourceSaver.save("%s.%s" % [save_path, get_save_extension()], scene)
